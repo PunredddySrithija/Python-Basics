ac32539c2e35de614bd21895338cb6f6fbbf6896
python <punreddysrithija531@gmail.com>
2024-07-20 11:33:41 +0530

    Datatypes

---
+++ DataTypes.py
@@ -0,0 +1,120 @@
+# This program prints Hello, world!
+
+print('Hello, world!')
+
+
+# This program adds two numbers
+
+num1 = 3.5
+num2 = 5.5
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+# mutliple variables
+
+A , B , C = ("Charan" , "Mahesh" , "Pavan")
+
+print(A)
+print(B)
+print(C)
+
+rohit=surya=kohli = "cricketers"
+print(rohit)
+print(surya)
+print(kohli)
+
+python=java=c = 'languages'
+print(python)
+print(java)
+print(c)
+
+
+# Program to generate a random number between 0 and 9
+
+# importing the random module The input() function takes input from the user and returns it.
+import random
+
+print(random.randint(0,99))
+
+
+import random
+
+my_list = [1, 'a', 32, 'c', 'd', 31]
+print(random.choice(my_list))
+
+
+# The input() function takes input from the user and returns it.
+
+name = input("Enter your name: ")
+print(name)
+
+
+# Arithmetic Operators in Python
+a = 4
+b = 5
+
+# addition
+print ('Sum: ', a + b)  
+
+# subtraction
+print ('Subtraction: ', a - b)   
+
+# multiplication
+print ('Multiplication: ', a * b)  
+
+# division
+print ('Division: ', a / b) 
+
+# floor division
+print ('Floor Division: ', a // b)
+
+# modulo
+print ('Modulo: ', a % b)  
+
+# a to the power b
+print ('Power: ', a ** b)
+
+# Python Comparison Operators
+a = 5
+b = 2
+
+# equal to operator
+print('a == b =', a == b)
+
+# not equal to operator
+print('a != b =', a != b)
+
+# greater than operator
+print('a > b =', a > b)
+
+# less than operator
+print('a < b =', a < b)
+
+# greater than or equal to operator
+print('a >= b =', a >= b)
+
+# less than or equal to operator
+print('a <= b =', a <= b)
+
+
+# examples of creating variables
+
+name = 'jayanth'
+city = 'dubai'
+age  = 26
+print(name)
+print(city)
+print(age)
+
+state = 'telangana'
+capital = 'hyderabad'
+country = 'india'
+money = 'rupee'
+print(state)
+print(capital)
+print(country)
+print(money)
